b69e551b2456bd12b934542d63bd02ebd52cc438
Antoine C <antoine.cormerais@epita.fr>
2017-05-29 16:53:15 +0200

    [WIP] js: check the token on the server side

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const fs = require('fs');
 const minify = require('express-minify');
+const admin = require("firebase-admin");
+const util = require('util')
+
+
+
+const serviceAccount = require("./cred.json");
+
+
 
 const app = express();
 
@@ -16,8 +24,26 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Methods", ["GET, POST, OPTIONS, PUT, DELETE"]);
   next();
 });
+app.post('/tokensignin',function(request,response){
+  console.log("toekn is : " + request.body.token);
+  admin.auth().verifyIdToken(request.body.token)
+  .then(function(decodedToken) {
+    var uid = decodedToken.uid;
+    console.log("Token is correct")
+    // ...
+  }).catch(function(error) {
+    // Handle error
+    console.log("Token is NOT correct")
+  });
 
+});
 // start your server
 app.listen(4242, () => {
   console.log('PlugMeme app listening on port 4242!');
+});
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://plugmeme-96341.firebaseio.com"
 });

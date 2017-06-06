67447c36bbcaa81a160d0a0b0b035fa3afb868a3
Arthur Lehuen <arthur@lehuen.net>
2017-06-06 17:47:32 +0200

    Clean Api utils

--- frontend/src/apiUtil.js
+++ frontend/src/apiUtil.js
@@ -1,46 +1,37 @@
 /**
  * Classe utilitaire de connection l'api PlugMeme
  */
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+
+  const error = new Error(response.statusText);
+  error.response = response;
+  throw error;
+};
+
 class PMApiClient {
-    constructor(token, apiUrl) {
-        this.token = token;
-        if(!apiUrl) {
-            this.apiUrl = "http://localhost:4242";
-        }
-        else {
-            this.apiUrl = apiUrl;
-        }
-    }
-    checkStatus(response) {
-        if (response.status >= 200 && response.status < 300) {
-            return response
-         } else {
-            var error = new Error(response.statusText)
-            error.response = response
-            throw error
-        }
-    }
- 
-    parseJSON(response) {
-    return response.json()
-}
+  constructor(token, apiUrl) {
+    this.token = token;
+    this.apiUrl = apiUrl || 'http://localhost:4242';
+  }
 
   api(method, params) {
-    let that = this;
-    return new Promise(function(resolve, reject) {
-        fetch(that.apiUrl + method, {
-                method: 'POST',
-                headers: {
-                'Content-Type': 'application/json',
-                'Authorization': that.token
-                },
-                body: JSON.stringify(params),
-            }).then(that.checkStatus).then(that.parseJSON)
-        .then(function(data) {
-            resolve(data);
-        }).catch(function(error) {
-            reject(error);
-        })
+    const that = this;
+    return new Promise((resolve, reject) => {
+      fetch(that.apiUrl + method, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: that.token,
+        },
+        body: JSON.stringify(params),
+      })
+      .then(checkStatus)
+      .then(response => response.json())
+      .then(data => resolve(data))
+      .catch(error => reject(error));
     });
   }
 }

ecfe6ad806d3498d57233d584f03b6b0d6552ac0
Arthur Lehuen <arthur@lehuen.net>
2017-06-07 02:06:43 +0200

    Create user in DB

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,28 +1,47 @@
 const mysql = require('mysql');
 const mysqlCred = require('./../mysqlCred.json');
 
+const con = mysql.createConnection(mysqlCred);
+con.connect((err) => {
+  if (err) throw err;
+});
+
+const sqlInit = "CREATE TABLE IF NOT EXISTS users (id VARCHAR(255) PRIMARY KEY, name VARCHAR(255))";
+const sqlSelectUser = 'SELECT * FROM users WHERE id = ?';
+const sqlCreateUser = `INSERT INTO users (id, name) VALUES (?, ?)`;
+
 const initDatabase = () => {
-  const sql = "CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), uid VARCHAR(255))";
-  
-  const con = mysql.createConnection(mysqlCred);
-  con.connect((err) => {
+
+  con.query(sqlInit, (err, result) => {
     if (err) throw err;
-    con.query(sql, (err, result) => {
-      if (err) throw err;
-      console.log('Init done');
-    });
   });
 }
 
 const query = () => {
-  const con = mysql.createConnection(mysqlCred);
-  con.connect((err) => {
-    if (err) throw err;
-    console.log('Connected');
-  });
 };
+
+const getUser = (id) => {
+  return new Promise((resolve, reject) => {
+    con.query(sqlSelectUser, [id] , (err, result) => {
+      if (err) reject(err);
+      if (!result.length) {
+        const rand = Math.floor(Math.random() * 10000) + 1;
+        const name = `anonymous${rand}`;
+        con.query(sqlCreateUser, [id, name] , (err, result) => {
+          if (err) reject(err);
+          resolve({
+            id,
+            name,
+          });
+        });
+      } else {
+        resolve(result[0]);
+      }
+    });
+  });
+}
 
 module.exports = {
   initDatabase,
-  query,
+  getUser,
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -25,14 +25,27 @@ var apiRoutes = express.Router();
 db.initDatabase();
 
 app.get('/api/tokensignin', firebaseAuth.checkToken, function(request,response) {
-  let user = request.params.user.toJSON();
-  user.username = "SuperPlop";
+  const user = request.params.user.toJSON();
 
-  db.query();
-
-  response.json(user);
-
+  db.getUser(user.uid)
+    .then(data => {
+      user.username = data.name;
+      response.json(user);          
+    })
+    .catch(err => console.log(err));
 });
+
+app.post('/api/user/name', firebaseAuth.checkToken, function(request,response) {
+  const user = request.params.user.toJSON();
+
+  db.getUser(user.uid)
+    .then(data => {
+      user.username = data.name;
+      response.json(user);          
+    })
+    .catch(err => console.log(err));
+});
+
 // start your server
 app.listen(4242, () => {
   console.log('PlugMeme app listening on port 4242!');
